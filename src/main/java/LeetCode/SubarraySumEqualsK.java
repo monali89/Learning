fb12271e8caea857d50689d8aa60ec28f8511084
Monali Lakhote <monalidl@gmail.com>
2020-05-03 15:49:04 -0400

    Subarray sum (sliding window) working solution

--- src/main/java/LeetCode/SubarraySumEqualsK.java
+++ src/main/java/LeetCode/SubarraySumEqualsK.java
@@ -8,15 +8,20 @@ import java.util.Queue;
 
 /**
  * @author: monali on 5/9/2019
+ * Problem: Given an array of integers and an integer k,
+ * you need to find the total number of continuous subarrays whose sum equals to k.
+ * Input:nums = [1,1,1], k = 2
+ * Output: 2
  */
 
 public class SubarraySumEqualsK {
 
-    @Test
-    public void test01(){
-        //Assert.assertEquals(2, subarraySum(new int[]{1,1,1}, 2));
-        //Assert.assertEquals(2, subarraySum(new int[]{1,2,1,3,0,2,1,4}, 5));
-        Assert.assertEquals(subarraySumTEST(new int[]{1,2,1,3,0,2,1,4}, 5), subarraySum(new int[]{1,2,1,3,0,2,1,4}, 5));
+    public static void main(String[] args) {
+        int[] input = {1,2,1,3,0,2,1,4};
+        System.out.println(subarraySum(input, 5));
+        System.out.println();
+        input = new int[]{1,1,1};
+        System.out.println(subarraySum(input, 2));
     }
 
     public static int subarraySum(int[] nums, int k) {
@@ -39,29 +44,4 @@ public class SubarraySumEqualsK {
         return count;
     }
 
-    public static int subarraySumTEST(int[] nums, int k) {
-
-        int count = 0;
-
-        for (int i = 0; i < nums.length; i++) {
-            int sum = 0;
-            int j = i;
-            while(j < nums.length && sum <= k){
-                sum = sum + nums[j++];
-            }
-            if(sum == k) count++;
-        }
-
-        return count;
-    }
-
 }
-
-
-/*
-* Given an array of integers and an integer k, you need to find the total number of continuous subarrays whose sum equals to k.
-*
-* Input:nums = [1,1,1], k = 2
-* Output: 2
-*
-* */

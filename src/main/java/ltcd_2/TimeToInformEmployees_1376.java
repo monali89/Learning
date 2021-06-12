178b9c0e673e750c551a5c48a47926d472bceba7
Monali Lakhote <monalidl@gmail.com>
2021-06-12 18:00:45 -0400

    1376 working brute force

--- src/main/java/ltcd_2/TimeToInformEmployees_1376.java
+++ src/main/java/ltcd_2/TimeToInformEmployees_1376.java
@@ -11,45 +11,33 @@ import java.util.Queue;
 
 public class TimeToInformEmployees_1376 {
 
+    private int[] m;
+    private int[] t;
+    private int n;
+
     public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
 
         int totalTime = 0;
 
-        // create a list of list
-        List<List<Integer>> ll = new ArrayList<List<Integer>>();
+        this.n = n;
+        this.m = manager;
+        this.t = informTime;
 
-        while (true) {
-            List<Integer> mangs = new ArrayList<Integer>();
-            List<Integer> subs = new ArrayList<Integer>();
+        totalTime = getTime(headID);
 
-            if (ll.size() == 0) {
-                mangs.add(headID);
-                ll.add(mangs);
-            } else {
-                mangs = ll.get(ll.size()-1);
-                for (int i = 0; i < manager.length; i++) {
-                    if (mangs.contains(manager[i])) {
-                        if (informTime[i] != 0) subs.add(i);
-                    }
-                }
-                if (subs.size() == 0) break;
-                ll.add(subs);
-            }
-        }
-        System.out.println(ll);
-
-        int prevMax = 0;
-        while (ll.size() > 0) {
-            int currMax = 0;
-            for (int node: ll.get(ll.size()-1)) {
-                currMax = Math.max(currMax, informTime[node]);
-            }
-            prevMax = prevMax + currMax;
-            ll.remove(ll.size()-1);
-        }
-
-        totalTime = prevMax;
         return totalTime;
+    }
+
+    private int getTime(int currManager) {
+        if (t[currManager] == 0) return 0;
+
+        int totalTime = 0;
+        for (int i = 0; i < n; i++) {
+            if (m[i] == currManager) {
+                totalTime = Math.max(totalTime, getTime(i));
+            }
+        }
+        return totalTime + t[currManager];
     }
 
     public static void main(String[] args) {
@@ -59,7 +47,7 @@ public class TimeToInformEmployees_1376 {
         int[] manager;
         int[] informTime;
 
-        /*n = 1; headID = 0;
+        n = 1; headID = 0;
         manager = new int[]{-1};
         informTime = new int[]{0};
         System.out.println(object.numOfMinutes(n, headID, manager, informTime));
@@ -87,7 +75,7 @@ public class TimeToInformEmployees_1376 {
         n = 10; headID = 3;
         manager = new int[]{8,9,8,-1,7,1,2,0,3,0};
         informTime = new int[]{224,943,160,909,0,0,0,643,867,722};
-        System.out.println(object.numOfMinutes(n, headID, manager, informTime));*/
+        System.out.println(object.numOfMinutes(n, headID, manager, informTime));
 
         n = 11; headID = 4;
         manager = new int[]{5,9,6,10,-1,8,9,1,9,3,4};

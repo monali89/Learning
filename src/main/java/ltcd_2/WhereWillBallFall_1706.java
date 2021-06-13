f5c13399adcd44053950b3cef6f9004a069f3789
Monali Lakhote <monalidl@gmail.com>
2021-06-13 10:58:09 -0400

    1706 accepted

--- src/main/java/ltcd_2/WhereWillBallFall_1706.java
+++ src/main/java/ltcd_2/WhereWillBallFall_1706.java
@@ -1,62 +1,89 @@
 package ltcd_2;
 
 public class WhereWillBallFall_1706 {
-    
+
     public int[] findBall(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
+
+        int m = grid.length; // rows
+        int n = grid[0].length; // cols
+
         int[] answer = new int[n];
 
-        String pos = null;
-
-        for (int k = 0; k < n; k++) {
-
-            int i = 0;
-            int j = k;
-            boolean[][] isVisited = new boolean[m][n];
-
-            while (i < m) {
-
-                if (isVisited[i][j]) {
-                    answer[k] = -1;
-                    break;
-                }
-                isVisited[i][j] = true;
-
-                if (grid[i][j] == 1) {
-
-                    // when current position is top right
-                    if (pos == null || pos.equals("bl") || pos.equals("br") || pos.equals("tl")) {
-                        pos = "tr";
-                        j++;
-                    } else if (pos.equals("tr")) {
-                        pos = "bl";
-                        if (i == m-1) {
-                            answer[k] = j;
-                            break;
-                        }
-                        i++;
+        for (int i = 0; i< n; i++) { // drop ball from each col
+            int r = 0;
+            int c = i;
+            while (r < m && c > -1 && c < n) {
+                if (grid[r][c] == 1) {
+                    if ((c+1) < n && grid[r][c+1] == 1) {
+                        r = r+1;
+                        c = c+1;
+                    } else {
+                        answer[i] = -1;
+                        break;
                     }
-                } else if (grid[i][j] == -1) {
-
-                    if (pos == null || pos.equals("bl") || pos.equals("br") || pos.equals("tr")) {
-                        pos = "tl";
-                        j--;
-                    } else if (pos.equals("tl")) {
-                        pos = "br";
-                        if (i == m-1) {
-                            answer[k] = j;
-                            break;
-                        }
-                        i++;
+                } else if (grid[r][c] == -1) {
+                    if ((c-1) > -1 && grid[r][c-1] == -1) {
+                        r = r + 1;
+                        c = c - 1;
+                    } else {
+                        answer[i] = -1;
+                        break;
                     }
                 }
-                if (j < 0 || j >= n) {
-                    answer[k] = -1;
-                    break;
-                }
+                answer[i] = c;
             }
         }
         return answer;
+    }
+
+    public static void main(String[] args) {
+        WhereWillBallFall_1706 object = new WhereWillBallFall_1706();
+        int[][] input;
+        int[] output;
+
+        // example 1
+        /*input = new int[][] {
+                {1,1,1,-1,-1},{1,1,1,-1,-1},{-1,-1,-1,1,1},{1,1,1,1,-1},{-1,-1,-1,-1,-1}
+        };
+        output = object.findBall(input);
+        System.out.println("Expected: [1,-1,-1,-1,-1]");
+        for (int value : output) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+
+        // example 2
+        input = new int[][] {
+                {-1}
+        };
+        output = object.findBall(input);
+        System.out.println("Expected: [-1]");
+        for (int value : output) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+
+        // example 3
+        input = new int[][] {
+                {1,1,1,1,1,1},{-1,-1,-1,-1,-1,-1},{1,1,1,1,1,1},{-1,-1,-1,-1,-1,-1}
+        };
+        output = object.findBall(input);
+        System.out.println("Expected: [0,1,2,3,4,-1]");
+        for (int value : output) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+*/
+        // after submit test
+        input = new int[][] {
+                {-1,1,-1,-1,-1,-1,-1,-1,1,-1,-1,-1,-1,1,1,-1,-1,-1,1,1,1,-1,-1,1,1,-1,-1,1,-1,-1,-1,-1,-1,-1,-1,-1,-1,1,-1,1,-1,-1,-1,-1,-1,-1,-1,1,-1,-1,1,-1,1,-1,-1,1,1,-1,1,-1,-1,-1,-1,1,1,1,1,1,1,-1,1,1,1,-1,1,1,1,-1,-1,-1,1,-1,1,-1,-1,1,1,-1,-1,1,-1,1,-1,1,1,1,-1,-1,-1,-1}
+        };
+        output = object.findBall(input);
+        System.out.println("Expected: TBD");
+        for (int value : output) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+
     }
 }

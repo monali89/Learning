16a161c79e0e065cb29425e2340ece88551c8db7
Monali Lakhote <monalidl@gmail.com>
2021-06-17 17:38:59 -0400

    149 logic comments

--- src/main/java/ltcd/MaxPointsLine_149.java
+++ src/main/java/ltcd/MaxPointsLine_149.java
@@ -1,8 +1,5 @@
 package ltcd;
 
-import org.junit.Assert;
-import org.junit.Test;
-
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -10,14 +7,17 @@ import java.util.Set;
 
 public class MaxPointsLine_149 {
 
-    @Test
-    public void test01(){
-        int[][] input = {{84,250},{0,0},{1,0},{0,-70},{0,-70},{1,-1},{21,10},{42,90},{-42,-230}};
-        Assert.assertEquals(6, maxPoints(input));
-    }
-
+    // slope of a line is y diff / x diff.
+    // For two points (x1, y1) and (x2, y2),
+    // the slope would be slope = (y2 - y1) / (x2 - x1)
 
     public int maxPoints(int[][] points) {
+
+        // Create a map storage which keeps track of slopes between all the points.
+        // Calculate slopes between all the points (using a nested for loop).
+        // Keep track slope and points in the map. The points for each slope are
+        // stored in set (so that duplicate points can be skipped).
+        // The set with largest size will be the result
 
         if(points == null) return 0;
         if(points.length <= 2) return points.length;
@@ -27,7 +27,7 @@ public class MaxPointsLine_149 {
 
         for(int i=0; i<points.length; i++){
             for(int j=i+1; j<points.length; j++){
-                Double slope;
+                double slope;
                 if(points[i][1] == points[j][1]){
                     slope = 1.0;
                 }else if(points[i][0] == points[j][0]){
@@ -69,42 +69,37 @@ public class MaxPointsLine_149 {
         input = new int[][] {{1,1},{2,2},{3,3}};
         expected = 3;
         output = object.maxPoints(input);
-        System.out.printf("Expected: %s | Output: %s | Result: %s", expected, output, (expected == output));
-        System.out.println();
+        System.out.printf("Expected: %s | Output: %s | Result: %s\n", expected, output, (expected == output));
 
         // example 2
         input = new int[][] {{1,1},{3,2},{5,3},{4,1},{2,3},{1,4}};
         expected = 4;
         output = object.maxPoints(input);
-        System.out.printf("Expected: %s | Output: %s | Result: %s", expected, output, (expected == output));
-        System.out.println();
+        System.out.printf("Expected: %s | Output: %s | Result: %s\n", expected, output, (expected == output));
 
         // after submitting 1
         input = new int[][] {{1,0},{0,0}};
         expected = 2;
         output = object.maxPoints(input);
-        System.out.printf("Expected: %s | Output: %s | Result: %s", expected, output, (expected == output));
-        System.out.println();
+        System.out.printf("Expected: %s | Output: %s | Result: %s\n", expected, output, (expected == output));
 
         // after submitting 2
         input = new int[][] {{0,0},{1,1}};
         expected = 2;
         output = object.maxPoints(input);
-        System.out.printf("Expected: %s | Output: %s | Result: %s", expected, output, (expected == output));
-        System.out.println();
+        System.out.printf("Expected: %s | Output: %s | Result: %s\n", expected, output, (expected == output));
 
         // after submitting 3
         input = new int[][] {{4,5},{4,-1},{4,0}};
         expected = 3;
         output = object.maxPoints(input);
-        System.out.printf("Expected: %s | Output: %s | Result: %s", expected, output, (expected == output));
-        System.out.println();
+        System.out.printf("Expected: %s | Output: %s | Result: %s\n", expected, output, (expected == output));
 
         // after submitting 4
         input = new int[][] {{84,250},{0,0},{0,-70},{-42,-230},{21,10},{42,90},{1,0},{1,-1}};
         expected = 5;
         output = object.maxPoints(input);
-        System.out.printf("Expected: %s | Output: %s | Result: %s", expected, output, (expected == output));
-        System.out.println();
+        System.out.printf("Expected: %s | Output: %s | Result: %s\n", expected, output, (expected == output));
     }
+
 }

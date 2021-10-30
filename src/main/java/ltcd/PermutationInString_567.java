aa386835fc162593280db18c00d764e584eaaac7
Monali Lakhote <monalidl@gmail.com>
2021-10-30 15:28:42 -0400

    updates

---
+++ src/main/java/ltcd/PermutationInString_567.java
@@ -0,0 +1,71 @@
+package ltcd;
+
+import common.Common;
+import org.junit.Assert;
+
+public class PermutationInString_567 {
+
+    public boolean checkInclusion(String s1, String s2) {
+
+        int[] s1Arr = new int[26];
+
+        for (char c: s1.toCharArray()) {
+            s1Arr[c - 'a']++;
+        }
+
+        int i = 0;
+        boolean flag = false;
+
+        while (i < s2.length()) {
+            char c = s2.charAt(i);
+
+            if (s1Arr[c - 'a'] > 0) {
+
+                int[] s2Arr = new int[26];
+
+                while (i < s2.length() && s1Arr[s2.charAt(i) - 'a'] > 0) {
+                    // found a char which is in s1
+                    s2Arr[s2.charAt(i) - 'a']++;
+                    i++;
+                }
+
+                int j = 0;
+                while (j < 26 && s1Arr[j] == s2Arr[j]) j++;
+                if (j == 26) flag = true;
+
+            } else {
+                i++;
+            }
+        }
+
+        return flag;
+    }
+
+    public static void main(String[] args) {
+
+        PermutationInString_567 object = new PermutationInString_567();
+        String inputS1;
+        String inputS2;
+        boolean output;
+        boolean expected;
+
+        inputS1 = "ab";
+        inputS2 = "eidbaooo";
+        output = object.checkInclusion(inputS1, inputS2);
+        expected = true;
+        Common.printResult(inputS1 + ", " + inputS2, output, expected);
+
+        inputS1 = "ab";
+        inputS2 = "eidboaoo";
+        output = object.checkInclusion(inputS1, inputS2);
+        expected = false;
+        Common.printResult(inputS1 + ", " + inputS2, output, expected);
+
+        inputS1 = "xy";
+        inputS2 = "eidboaoo";
+        output = object.checkInclusion(inputS1, inputS2);
+        expected = false;
+        Common.printResult(inputS1 + ", " + inputS2, output, expected);
+
+    }
+}
--- src/main/java/ltcd_2/HouseRobberII_213.java
+++ src/main/java/ltcd_2/HouseRobberII_213.java
@@ -1,43 +1,84 @@
 package ltcd_2;
 
+import common.Common;
+
 /**
  * @author Monali L on 6/5/2021
  */
 public class HouseRobberII_213 {
 
-    private int[] memo;
-
     public int rob(int[] nums) {
 
-        if (nums.length == 1) return nums[0];
-        if (nums.length == 2) return Math.max(nums[0], nums[1]);
+        int len = nums.length;
 
-        memo = new int[nums.length];
-        memo[0] = nums[0];
-        memo[1] = nums[1];
-        helper(nums.length-1, nums);
+        if (len <= 0 || len == 2) return 0;
+        if (len == 1) return nums[0];
 
-        for (int i = 0; i < memo.length; i++) {
-            System.out.println(memo[i]);
+        int[] dpIncludeFirst = new int[len + 1];
+        int[] dpExcludeFirst = new int[len + 1];
+
+        dpIncludeFirst[0] = nums[0];
+        dpIncludeFirst[1] = nums[1];
+
+        for (int i=2; i<len-1; i++) {
+            int max = nums[i];
+            for (int j=i-2; j>=0; j--) {
+                max = Math.max(max, nums[i] + dpIncludeFirst[j]);
+            }
+            dpIncludeFirst[i] = max;
         }
 
-        if (nums.length % 2 == 0) {
-            return memo[nums.length - 1];
-        } else {
-            return memo[nums.length - 2];
-        }
-    }
+        dpIncludeFirst[len] = Math.max(dpIncludeFirst[len-2], dpIncludeFirst[len-3]);
 
-    public int helper(int i, int[] nums) {
-        if (i < 0) return 0;
-        if (memo[i] != 0) return memo[i];
-        memo[i] = Math.max( helper(i-1, nums) , nums[i] + helper(i-2, nums) );
-        return memo[i];
+        dpExcludeFirst[0] = 0;
+        dpExcludeFirst[1] = nums[1];
+
+        for (int i=2; i<len; i++) {
+            int max = nums[i];
+            for (int j=i-2; j>0; j--) {
+                max = Math.max(max, nums[i] + dpExcludeFirst[j]);
+            }
+            dpExcludeFirst[i] = max;
+        }
+
+        dpExcludeFirst[len] = Math.max(dpExcludeFirst[len-1], dpExcludeFirst[len-2]);
+
+        System.out.println("dpIncludeFirst: " + Common.arrayToString(dpIncludeFirst));
+        System.out.println("dpExcludeFirst: " + Common.arrayToString(dpExcludeFirst));
+
+        return Math.max(dpIncludeFirst[len], dpExcludeFirst[len]);
     }
 
     public static void main(String[] args) {
+
         HouseRobberII_213 object = new HouseRobberII_213();
-        int[] input = new int[]{1,3,1,3,100};
-        System.out.println("Answer: " + object.rob(input)); // 4
+        int[] input;
+        int output;
+        int expected;
+
+        input = new int[]{2,3,2};
+        output = object.rob(input);
+        expected = 3;
+        Common.printResult(Common.arrayToString(input), output, expected);
+
+        input = new int[]{1,2,3,1};
+        output = object.rob(input);
+        expected = 4;
+        Common.printResult(Common.arrayToString(input), output, expected);
+
+        input = new int[]{1,2,3};
+        output = object.rob(input);
+        expected = 3;
+        Common.printResult(Common.arrayToString(input), output, expected);
+
+        input = new int[]{1,3,1,3,100};
+        output = object.rob(input);
+        expected = 103;
+        Common.printResult(Common.arrayToString(input), output, expected);
+
+        input = new int[] {200,3,140,20,10};
+        output = object.rob(input);
+        expected = 340;
+        Common.printResult(Common.arrayToString(input), output, expected);
     }
 }

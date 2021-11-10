4b21fb8de4c500558d9fdc2838ec91e3b6a16b41
Monali Lakhote <monalidl@gmail.com>
2021-11-10 17:45:34 -0500

    updates

--- src/main/java/ltcd_2/UglyNumberII_264.java
+++ src/main/java/ltcd_2/UglyNumberII_264.java
@@ -10,21 +10,28 @@ public class UglyNumberII_264 {
     public int nthUglyNumber(int n) {
 
         if (n == 1) return 1;
-        List<Integer> dp = new ArrayList<>(n);
-        dp.add(1);
+        int[] dp = new int[n];
+        dp[0] = 1;
 
-        int i = 2;
-        while (dp.size() != n) {
-            if (isUgly(i)) {
-                dp.add(i);
+        int index2 = 1, index3 = 1, index5 = 1;
+        int factor2 = 2, factor3 = 3, factor5 = 5;
+
+        for (int i = 1; i < n; i++) {
+            int min = Math.min( Math.min(factor2, factor3), factor5);
+            dp[i] = min;
+
+            if (min == factor2) {
+                factor2 = 2 * dp[index2++];
             }
-            System.out.println(i + " - " + (isUgly(i) ? "Ugly" : "NU"));
-            i++;
+            if (min == factor3) {
+                factor3 = 3 * dp[index3++];
+            }
+            if (min == factor5) {
+                factor5 = 5 * dp[index5++];
+            }
         }
 
-        // System.out.println(dp);
-
-        return dp.get(dp.size() - 1);
+        return dp[n-1];
     }
 
     private boolean isUgly(int n) {

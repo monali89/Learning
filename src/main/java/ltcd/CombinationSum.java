f1b8c7f9f33db3c86a306dacdd33df0ddd04e464
Monali Lakhote <monalidl@gmail.com>
2021-11-15 19:44:59 -0500

    updates

--- src/main/java/ltcd/CombinationSum.java
+++ src/main/java/ltcd/CombinationSum.java
@@ -1,21 +1,77 @@
 package ltcd;
 
+import common.Common;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class CombinationSum {
 
     public static void main(String[] args) {
-        System.out.println(combinationSum(new int[]{2,3,5}, 8));
+
+        CombinationSum object = new CombinationSum();
+
+        // System.out.println(object.combinationSum(new int[]{2,3,5}, 8));
+        System.out.println(object.combinationSum4(new int[] {1,2,3}, 4));
+
     }
 
-    public static List<List<Integer>> combinationSum(int[] candidates, int target) {
+    // 377. Combination Sum IV: Given an array of distinct
+    // integers nums and a target integer target, return the
+    // number of possible combinations that add up to target.
+
+    private int noOfComb;
+    private int[] dp;
+
+    public int combinationSum4(int[] nums, int target) {
+        noOfComb = 0;
+        dp = new int[target+1];
+        Arrays.fill(dp, -1);
+        combinationSum4_helper(nums, target);
+        System.out.println(Common.arrayToString(dp));
+        return noOfComb;
+    }
+
+    private void combinationSum4_helper(int[] nums, int rem) {
+
+        if (rem == 0) noOfComb++;
+        if (rem < 0) return;
+        if (dp[rem] != 1) return;
+
+        for (int i = 0; i < nums.length; i++) {
+            combinationSum4_helper(nums, rem - nums[i]);
+        }
+
+        System.out.println("rem:" + rem + ", combos: " + noOfComb);
+        dp[rem] = noOfComb;
+    }
+
+
+    /*
+     * Given a set of candidate numbers (candidates) (without duplicates) and a target number (target),
+     * find all unique combinations in candidates where the candidate numbers sums to target.
+     * The same repeated number may be chosen from candidates unlimited number of times.
+     *
+     * All numbers (including target) will be positive integers.
+     * The solution set must not contain duplicate combinations.
+     *
+     * Input: candidates = [2,3,6,7], target = 7,
+     * A solution set is:
+     * [
+     *   [7],
+     *   [2,2,3]
+     * ]
+     *
+     * */
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> list = new ArrayList<List<Integer>>();
         backtrack(list, new ArrayList<Integer>(), candidates, target, 0);
         return list;
     }
 
-    public static void backtrack(List<List<Integer>> final_list, List<Integer> this_list, int[] nums, int remaining, int start){
+    public void backtrack(List<List<Integer>> final_list, List<Integer> this_list, int[] nums, int remaining, int start){
         if(remaining == 0){
             final_list.add(new ArrayList<Integer>(this_list));
         }else if(remaining < 0){
@@ -30,20 +86,3 @@ public class CombinationSum {
     }
 
 }
-
-/*
-* Given a set of candidate numbers (candidates) (without duplicates) and a target number (target),
-* find all unique combinations in candidates where the candidate numbers sums to target.
-* The same repeated number may be chosen from candidates unlimited number of times.
-*
-* All numbers (including target) will be positive integers.
-* The solution set must not contain duplicate combinations.
-*
-* Input: candidates = [2,3,6,7], target = 7,
-* A solution set is:
-* [
-*   [7],
-*   [2,2,3]
-* ]
-*
-* */
--- src/main/java/ltcd_2/CoinChange.java
+++ src/main/java/ltcd_2/CoinChange.java
@@ -5,15 +5,54 @@ import common.Common;
 public class CoinChange {
 
     // 518. Coin Change 2
-    public int change(int amount, int[] coins) {
 
+    private int combinations;
+
+    public int change(int amount, int[] coins) {
+        helper_2(coins, amount);
+        return combinations;
     }
+
+    private void helper_2(int[] coins, int remaining) {
+        if (remaining == 0) combinations++;
+        else if (remaining < 0) return;
+        else {
+            for (int i = 0; i < coins.length; i++) {
+                helper_2(coins, remaining - coins[i]);
+            }
+        }
+    }
+
 
     // 322. Coin Change
     public int coinChange(int[] coins, int amount) {
         if (amount < 1) return 0;
         return helper(coins, amount, new int[amount]);
 
+    }
+
+    private int[] coinChangeRecursiveDP;
+    public int coinChange_Recursive(int[] coins, int amount) {
+        coinChangeRecursiveDP = new int[amount + 1];
+        return coinChange_Recursive_Helper(coins, amount);
+    }
+
+    public int coinChange_Recursive_Helper(int[] coins, int amount) {
+        if (amount < 0) return -1;
+
+        if (coinChangeRecursiveDP[amount] != -1)
+            return coinChangeRecursiveDP[amount];
+
+        if (amount == 0) return 0;
+        int v = Integer.MAX_VALUE;
+        for (int i = 0; i < coins.length; i++) {
+            int currCoins = 1 + coinChange_Recursive_Helper(coins, amount - coins[i]);
+            v = Math.min(currCoins, v);
+        }
+
+        coinChangeRecursiveDP[amount] = v == Integer.MAX_VALUE ? -1 : v;
+
+        return v;
     }
 
     private int helper(int[] coins, int rem, int[] count) {
@@ -42,13 +81,13 @@ public class CoinChange {
         int output;
         int expected;
 
-        coins = new int[] {1,2,5};
+        /*coins = new int[] {1,2,5};
         amount = 11;
         output = object.coinChange(coins, amount);
         expected = 3;
         Common.printResult(Common.arrayToString(coins) + " | " + amount, output, expected);
 
-        /*coins = new int[] {2,5,10,1};
+        coins = new int[] {2,5,10,1};
         amount = 27;
         output = object.coinChange(coins, amount);
         expected = 4;
@@ -65,5 +104,14 @@ public class CoinChange {
         output = object.coinChange(coins, amount);
         expected = 20;
         Common.printResult(Common.arrayToString(coins) + " | " + amount, output, expected);*/
+
+        /*coins = new int[] {1,2,5};
+        amount = 5;
+        output = object.change(5, coins);
+        expected = 4;
+        Common.printResult(Common.arrayToString(coins) + " | " + amount, output, expected);*/
+
+        // test coin change recursive
+        System.out.println(object.coinChange_Recursive(new int[] {1,2,5}, 7));
     }
 }
